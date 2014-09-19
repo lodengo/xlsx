c4b34b149ee0eb94ccb9e44c52dcf5e730a09e11
lodengo <ddl5200@gmail.com>
2014-09-19 14:55:23 +0800

    copy sheet with chart

--- test/test.js
+++ test/test.js
@@ -7,12 +7,12 @@ function arrays_equal(a,b) { return !(a<b || b<a); }
 var xls = new xlsx();
 xls.loadFile('test.xlsx');
 //set sheet name
-var sheetsCount = xls.sheetsCount();
-xls.setSheetName(0, 'sheet新名');
+assert.equal(xls.sheetsCount(), 2);
+xls.setSheetName(0, 'aa');
 //get sheet
 var sheet0 = xls.getSheet(0);
 //write sheet
-sheet0.write('A1', [ 'string', '数字', 'boolean' ]);
+sheet0.write('A1', [ 'string', 'num', 'boolean' ]);
 sheet0.write('A2', [ [ 'aa', 30, true ], [ 'bb', 40, false ] ]);
 sheet0.write('A4', 'cc');
 //read sheet
@@ -23,34 +23,26 @@ assert(arrays_equal(sheet0.read('A2:A3'), [ 'aa', 'bb' ]));
 assert.equal(sheet0.read('B3'), 40);
 assert(arrays_equal(sheet0.read('A2:C3'), [ [ 'aa', 30, true ], [ 'bb', 40, false ] ]));
 assert.equal(sheet0.read('A4'), 'cc');
-//write more data
-var datas = randomRows(1000);
-sheet0.write('A5', datas, true);
-//copy sheet
-xls.copySheet(0, 'copy拷贝');
+
+//copy sheet with chart
+var sheet2 = xls.copySheet(1, 'bb');
+sheet2.write('A1', [40, 60, 20]);
+
+var sheet3 = xls.copySheet(1, 'cc');
+sheet3.write('A1', [15, 23, 36]);
+
+//sheet count
+assert.equal(xls.sheetsCount(), 4);
+
+//hidden sheet
+xls.setSheetVisible(1, false);
+
 //write file
 xls.writeFile('out.xlsx');
 
-function randomRows(rs){
-	var rows = [];
-	for(var r = 0; r < rs; r++){
-		var row = [];
-		for(var c = 0; c < 10; c++){
-			row.push(Math.floor(Math.random() * 100));
-		}
-		rows.push(row);
-	}
-	return rows;
-}
+//express download 
+//app.get('/download', function(req, res){xls.download('aa', res);});
 
 console.log(['memoryUsage:',util.inspect(process.memoryUsage())]);
 console.log(['uptime', process.uptime()]);
-//download
-function download(xls, filename, httpServerResponse) {
-	httpServerResponse.writeHead(200, {
-		'Content-Type' : 'application/vnd.openxmlformats',
-		'Content-Disposition' : 'attachment; filename="'
-				+ encodeURIComponent(filename) + '.xlsx"'
-	});
-	httpServerResponse.end(xls.data());
-};
+
